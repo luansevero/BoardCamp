28f93fb8710ef8e237984c68793e3d24bfec0292
Luan Severo <luansevero@icloud.com>
2022-07-24 14:52:36 -0300

    [Feat] Game post route

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,20 +1,34 @@
-import e from "express";
 import connection from "../setup/database.js";
 
 const gamesController = {
     get: async function(req,res){
         try{
-            const { rows: games } = connection.query(`
+            const { rows:games } = connection.query(`
                 SELECT g.*, c.name as "categoryName" 
                 FROM games g 
                 JOIN categories c
-                ON g."categoryId"=c.id
+                ON g."categoryId" = c.id
             `)
             res.send(games)
         }catch(error){
             res.sendStatus(500);
         }
-    }
+    },
+    newGame: async function(req,res){
+        const {name, image, stockTotal, categoryId, pricePerDay} = req.body
+        try{
+            await connection.query(`
+                INSERT INTO games
+                (name, image, "stockTotal", "categoryId", "pricePerDay")
+                VALUES
+                ($1, $2, $3, $4, $5)`,
+                [name, image, stockTotal, categoryId, pricePerDay]
+            )
+            res.send(201)
+        }catch(error){
+            res.sendStatus(500);
+        }
+    },
 }
 
 export default gamesController
