649343d528de34fbdbcc1e57766d3ac48b2245e9
Luan Severo <luansevero@icloud.com>
2022-07-25 14:14:20 -0300

    [Feat] Logic to remove and addQueyString equal to the querys adding by user

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import gamesController from "../controllers/gamesController.js";
 import gamesMiddlewares from "../middlewares/gamesMiddlewares.js";
+import sharedQueryMiddlewares from "../middlewares/sharedQueryMiddleware.js";
 
 const gamesRouter = Router();
 
-gamesRouter.get("/games", gamesController.get);
+gamesRouter.get("/games", sharedQueryMiddlewares.removeInvalidQuery, sharedQueryMiddlewares.addQueryString, gamesController.get);
 gamesRouter.post("/games", gamesMiddlewares.validation, gamesMiddlewares.isNew,gamesMiddlewares.haveCategory, gamesController.newGame);
 
 export default gamesRouter;
